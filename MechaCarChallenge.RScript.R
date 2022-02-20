dcd7bb7206116c32a82e594539765e8e0b88b506
manBow1119 <62524752+manBow1119@users.noreply.github.com>
2022-02-20 16:09:30 -0600

    Update MechaCarChallenge.RScript.R

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -9,3 +9,25 @@ lm(mpg ~ AWD+ground_clearance+spoiler_angle+vehicle_weight+vehicle_length,mpg_ta
 
 #summarize linear model
 summary(lm(mpg ~ AWD+ground_clearance+spoiler_angle+vehicle_weight+vehicle_length,mpg_table))
+
+#import and read csv
+coil_table <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors=F)
+
+#create total summary dataframe
+total_summary <-coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
+ 
+#create lot groups for summarized data
+lot_summary = coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+#t-test for stat significance
+t.test(log10(coil_table$PSI), mu=mean(log10(total_summary$Mean))) 
+
+#t-test with subset
+#create subsets-- do we need these?
+coil_table_lot_1 = subset(coil_table, Manufacturing_Lot == "Lot1")
+coil_table_lot_2 = subset(coil_table, Manufacturing_Lot == "Lot2")
+coil_table_lot_3 = subset(coil_table, Manufacturing_Lot == "Lot3")
+
+t.test((coil_table_lot_1$PSI), mu=mean(coil_table$PSI)) 
+t.test((coil_table_lot_2$PSI), mu=mean(coil_table$PSI)) 
+t.test((coil_table_lot_3$PSI), mu=mean((coil_table$PSI))) 
